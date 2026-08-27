9b6d8e7e560bdb253d0f805378e91666d8d4e177
contiiiii <andreaconti050@gmail.com>
2026-08-27 12:08:02 +0200

    creazione struttura base frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import "tailwindcss";
+
+import Header from './components/header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center flex-col">
+      <h1 className="text-5xl font-bold text-lime-400  m-5">
+        CAGOMETRO 💩
+      </h1>
+    </div>
+
+  );
+}
