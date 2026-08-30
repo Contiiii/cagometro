997a478f2626f36969d357cb4ecd9f08736222fb
contiiiii <andreaconti050@gmail.com>
2026-08-30 19:56:50 +0200

    aggiunto PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,57 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        id: "/",
+
+        name: "Cagometro",
+        short_name: "Cagometro",
+
+        description: "Tracker personale delle registrazioni",
+
+        theme_color: "#000000",
+        background_color: "#000000",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        screenshots: [
+          {
+            src: "screenshot-mobile.png",
+            sizes: "1080x1920",
+            type: "image/png",
+          },
+          {
+            src: "screenshot-desktop.png",
+            sizes: "1920x1080",
+            type: "image/png",
+            form_factor: "wide",
+          },
+        ],
+
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
