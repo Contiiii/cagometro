2859680e3387630ee60a292a21ba84e490bc67e8
contiiiii <andreaconti050@gmail.com>
2026-08-30 23:37:38 +0200

    modificato header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,11 +2,66 @@ import poop from "../assets/poop.png";
 
 export default function Header() {
   return (
-    <div
-      className="flex items-center justify-center  w-full mt-15"
+    <header
+      className="
+        w-full
+        pt-6
+        pb-4
+        flex
+        justify-center
+      "
     >
-      <h1 className="text-4xl font-black text-pink-400 cursor-pointer hover:opacity-80 transition-all">CAGOMETRO </h1>
-      <img src={poop} alt="Logo Cagometro" className="h-14 object-contain ml-3" />
-    </div>
+      <div className="flex items-center gap-3">
+        <div
+          className="
+            flex
+            h-14
+            w-14
+            items-center
+            justify-center
+            rounded-full
+            border
+            border-pink-500/20
+            bg-pink-500/10
+          "
+        >
+          <img
+            src={poop}
+            className="
+                        text-4xl
+                        font-black
+                        tracking-tight
+                        text-pink-400
+                        h-8
+                        w-8"
+          />
+        </div>
+
+        <div>
+          <h1
+            className="
+              text-3xl
+              sm:text-4xl
+              font-black
+              tracking-tight
+              text-pink-400
+            "
+          >
+            CAGOMETRO
+          </h1>
+
+          <p
+            className="
+              text-xs
+              uppercase
+              tracking-[0.2em]
+              text-zinc-500
+            "
+          >
+            tracking professionale
+          </p>
+        </div>
+      </div>
+    </header>
   );
 }
