0f406a5aa66ac849325b08538e4e23f041baa13c
contiiiii <andreaconti050@gmail.com>
2026-08-30 23:43:34 +0200

    modificata undo botton

--- frontend/src/components/UndoBotton.jsx
+++ frontend/src/components/UndoBotton.jsx
@@ -1,23 +1,17 @@
-
-
-export default function UndoBotton({onClick}) {
+export default function UndoBotton({ onClick }) {
   return (
     <button
-    onClick={onClick}
-      className="bg-zinc-800/30
-                border
-                border-zinc-700/30
-                text-zinc-400
-                hover:bg-red-900/20
-                hover:border-red-500/30
-                hover:text-red-300
-                hover:scale-105
-                transition-all
-                duration-200
-                rounded-2xl
-                py-3
-                px-5
-                cursor-pointer">
+      type="button"
+      onClick={onClick}
+      className="
+        text-sm
+        text-zinc-500
+        transition-colors
+        duration-200
+
+        hover:text-red-300
+      "
+    >
       ↩ Annulla ultima registrazione
     </button>
   );
