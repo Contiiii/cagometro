ec9e83f38a1b914933dcb1abcd621882a70c5992
contiiiii <andreaconti050@gmail.com>
2026-09-11 15:00:34 +0200

    risolti bug vari

--- frontend/src/config/releaseNotes.js
+++ frontend/src/config/releaseNotes.js
@@ -1,51 +1,51 @@
 import { BarChart3, UsersRound } from "lucide-react";
 
-export const APP_VERSION = "1.8.3";
+export const APP_VERSION = "1.9.0";
 
 export const RELEASE_FEATURES = [
   {
-    id: "teams",
+    id: "teams-1",
     title: "Nuova classifica!",
     description:
       "Ora nella nuova classifica sotto al tuo nome ci saranno anche i tuoi XP e la tua streak!",
     icon: UsersRound,
   },
-  {
-    id: "reports",
-    title: "Share eport migliorati",
-    description:
-      "Nuovo design per lo shere dei report! e nuove animazioni per gli achievements",
-    icon: BarChart3,
-  },
-  {
-    id: "reports",
-    title: "periodo in tre righe",
-    description: "Nuovo design per la sezione periodo in tre righe!",
-    icon: BarChart3,
-  },
-  {
-    id: "reports",
-    title: "Grafici modificati!",
-    description:
-      "I grafici della settimana e del totale sono stati modificati! ora rappresenteranno rispettivamente dal lunedi alla domenica e almeno 3 grafici anche se nulli!",
-    icon: BarChart3,
-  },
-  {
-    id: "teams",
+    {
+    id: "teams-2",
     title: "Nuovo design della classifica!",
     description:
       "Ora i primi 3 della classifica del proprio team avranno un titolo speciale!",
     icon: UsersRound,
   },
-  {
-    id: "teams",
+    {
+    id: "teams-3",
     title: "Schermata di accesso da link!",
     description:
       "Nuova schermata di accesso al teams quando si entra con il link! e aggiunta opzione per disattivare il codice di invito",
     icon: UsersRound,
   },
   {
-    id: "reports",
+    id: "reports-1",
+    title: "Share eport migliorati",
+    description:
+      "Nuovo design per lo shere dei report! e nuove animazioni per gli achievements",
+    icon: BarChart3,
+  },
+  {
+    id: "reports-2",
+    title: "periodo in tre righe",
+    description: "Nuovo design per la sezione periodo in tre righe!",
+    icon: BarChart3,
+  },
+  {
+    id: "reports-3",
+    title: "Grafici modificati!",
+    description:
+      "I grafici della settimana e del totale sono stati modificati! ora rappresenteranno rispettivamente dal lunedi alla domenica e almeno 3 grafici anche se nulli!",
+    icon: BarChart3,
+  },
+  {
+    id: "reports-4",
     title: "Nuovo Popup!",
     description: "Nuovo popup motivazionale ad ogni registrazione!",
     icon: BarChart3,
